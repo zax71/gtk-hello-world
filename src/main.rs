4c084c0db3be96c2d7053a7abf960c3d2a2b287c
Zax71 <zabax32@gmail.com>
2024-04-10 13:16:54 +0100

    Initial app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{ glib, Application, ApplicationWindow };
+
+const APP_ID: &str = "me.zax71.HelloWorld";
+
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder().application(app).title("Hello GTK").build();
+
+    window.present();
 }
